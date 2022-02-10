5805790766b1569566a83fce1670ac0df19f40d1
fzyang1227 <62119227+fzyang1227@users.noreply.github.com>
2022-02-10 14:56:18 -0500

    shape and buttons

--- main.js
+++ main.js
@@ -10,5 +10,19 @@ Modified: 12/06/21
     // You will need to call this function when Button 1 is clicked
     // You will need to modiffy the body of this function as described in the assignment Readme
 function button1Clicked() {
-    console.log("Button 1 was clicked"); 
+    let randNum = Math.floor(Math.random()*100);
+    console.log("Button 1 was clicked");
+    let randomDiv = document.getElementById("random_num");
+    randomDiv.innerHTML = randNum;
 }
+
+function button2Clicked() {
+    let color = document.getElementById("circle1");
+    console.log("Button 2 was clicked");
+    console.log(color.style.cssText)
+    if (color.style.fill == "blue") {
+        color.style.fill = "green";
+    } else {
+        color.style.fill = "blue"
+    }
+}   
